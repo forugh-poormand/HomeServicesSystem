173ba008c49b66556b42e49831db4f1946edb679
forugh_poormand <forugh_poormand@yahoo.com>
2025-07-13 07:14:16 +0330

    add findByStatus in SpecialistRepository

--- src/main/java/ir/maktab127/homeservicessystem/repository/SpecialistRepository.java
+++ src/main/java/ir/maktab127/homeservicessystem/repository/SpecialistRepository.java
@@ -1,11 +1,14 @@
 package ir.maktab127.homeservicessystem.repository;
 
 import ir.maktab127.homeservicessystem.entity.Specialist;
+import ir.maktab127.homeservicessystem.entity.enums.SpecialistStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 
 public interface SpecialistRepository extends JpaRepository<Specialist, Long> {
-    public Optional<Specialist> findByEmail(String email);
+    Optional<Specialist> findByEmail(String email);
+    List<Specialist> findByStatus(SpecialistStatus status);
 }
